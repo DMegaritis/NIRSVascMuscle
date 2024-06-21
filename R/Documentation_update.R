c2872e083e0f6202167cd9bb78576a1379cc6256
dmegaritis <dmgrei3@gmail.com>
2024-06-21 16:49:43 +0100

    roxygen2 update

--- R/Documentation_update.R
+++ R/Documentation_update.R
@@ -1,7 +1,27 @@
+#' Set Working Directory and Document the Package
+#'
+#' This script sets the working directory to the root of the package,
+#' generates the documentation using roxygen2, and performs a package check.
+#'
+#' @description
+#' Set the working directory to the root of your package, generate the documentation,
+#' and check the package for any issues.
+#'
+#' @examples
+#' # Set the working directory to the root of your package
+#' setwd("/Users/dimitrismegaritis/Desktop/NIRSVascMusle")
+#'
+#' # Document the package
+#' devtools::document()
+#'
+#' # Check the package
+#' devtools::check()
+
 # Set working directory to the root of your package
 setwd("/Users/dimitrismegaritis/Desktop/NIRSVascMusle")
 
 # Document the package
 devtools::document()
 
+# Check the package
 devtools::check()
